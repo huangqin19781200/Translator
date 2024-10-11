b335d54e6bbfc6c84ed6d1798ada8e0f045a3688
HGG <huangqin19781200@163.com>
2024-10-12 04:12:21 +0800

    Delete config directory

--- config/__init__.py
+++
@@ -1,4 +0,0 @@
-import logging
-
-from .logging_settings import LoggingSettings
-from .settings import Settings
--- config/settings.py
+++
@@ -1,7 +0,0 @@
-class Settings():
-    # 请在下方填入您的SecretId和SecretKey,否则无法使用
-    # 若没有,请前往 https://cloud.tencent.com/ 获取您自己的SecretId和SecretKey,并注意保存
-    # 请不要泄露您的SecretId和SecretKey,否则可能导致您的账号财产安全受到威胁
-    
-    SecretId = ""
-    SecretKey = ""
