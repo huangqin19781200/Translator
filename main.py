63b03c2205ccc0b662a0310c85ee334fb7ee773d
HGG <huangqin19781200@163.com>
2024-10-13 22:48:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import sys
+import os
 
+from ui import *
 from config import *
 from models import *
-from ui_window import *
-from widgets import *
 
 
 class MainWindow(QMainWindow, LoggingSettings):
     def __init__(self) -> None:
-        # 初始化设置和窗口
         QMainWindow.__init__(self)
         LoggingSettings.__init__(self)
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        UiFunctions.ui_definitions(self)    # 初始化窗口设置
-
-        self.is_maximize = False    # 默认未最大化窗口
+        UiFunctions.ui_definitions(self)
+        UiFunctions.grip_definitions(self)
+        UiFunctions.tray_definitions(self)
 
         # 设置按钮连接
         self.ui.btnSet.clicked.connect(self.buttonClick)
@@ -25,30 +24,24 @@ class MainWindow(QMainWindow, LoggingSettings):
         self.ui.btnClearInput.clicked.connect(self.buttonClick)
         self.ui.btnClearOutput.clicked.connect(self.buttonClick)
 
-        self.ui.frameAppBackground.mouseMoveEvent = self.moveWindow                         # 将窗口背景与窗口移动方法绑定
-        self.ui.frameTopBackground.mouseDoubleClickEvent = self.dobleClickMaximizeRestore   # 将顶部栏与双击最大化/还原方法绑定
-
         self.show() # 显示窗口
-    
-    # 程序的按钮功能
+
     def buttonClick(self) -> None:
-        # 获取按钮名字
-        btn_name = self.sender().objectName()
+        btn_name = self.sender().objectName()   # 获取按钮名字
 
-        # 输出翻译
-        if btn_name == 'btnSet' or btn_name == 'btnAppend':
-            selecte = self.ui.comboxSelect.currentText()
-            source, target = AppFunctions.select_language(self, selecte)
+        # 覆盖到输出窗口
+        if btn_name == 'btnSet':
+            select = self.ui.comboxSelect.currentText()
+            source_text = self.ui.plainTextEditInput.toPlainText()
+            target_text =  AppFunctions.translate(self, source_text, select)
+            self.ui.plainTextEditOutput.setPlainText(target_text)
 
-            target_text = AppFunctions.translate(self, source, target)
-
-            # 覆盖到输出窗口
-            if btn_name == 'btnSet':
-                self.ui.plainTextEditOutput.setPlainText(target_text)
-
-            # 追加到输出窗口
-            if btn_name == 'btnAppend':
-                self.ui.plainTextEditOutput.appendPlainText(target_text)
+        # 追加到输出窗口
+        if btn_name == 'btnAppend':
+            select = self.ui.comboxSelect.currentText()
+            source_text = self.ui.plainTextEditInput.toPlainText()
+            target_text = AppFunctions.translate(self, source_text, select)
+            self.ui.plainTextEditOutput.appendPlainText(target_text)
 
         # 清空输入内容
         if btn_name == 'btnClearInput':
@@ -58,38 +51,48 @@ class MainWindow(QMainWindow, LoggingSettings):
         if btn_name == 'btnClearOutput':
             self.ui.plainTextEditOutput.clear()
 
-    # 检测鼠标事件, 并根据鼠标事件调整窗口大小及拖拽握把
-    def resizeEvent(self, event) -> None:
-        self.left_grip.setGeometry(0, 10, 10, self.height())
-        self.right_grip.setGeometry(self.width() - 10, 10, 10, self.height())
-        self.top_grip.setGeometry(0, 0, self.width(), 10)
-        self.bottom_grip.setGeometry(0, self.height() - 10, self.width(), 10)
-
-    # 检测鼠标按下事件, 获取鼠标指针在屏幕上的全局坐标，并赋值给变量
+    # 检测鼠标按下事件, 获取鼠标指针的坐标
     def mousePressEvent(self, event) -> None:
-        self.dragPos = event.globalPos()
+        self.dragPos = event.globalPosition().toPoint()
 
-    # 双击最大化/还原窗口
-    def dobleClickMaximizeRestore(self, event) -> None:
-        # 当双击顶部栏时
-        if event.type() == QEvent.MouseButtonDblClick:
-            QTimer.singleShot(250, lambda: UiFunctions.maximize_restore(self))
+    def resizeEvent(self, event) -> None:
+        UiFunctions.resize_grips(self)
 
-    # 设置窗口拖动
-    def moveWindow(self, event) -> None:
-        # 当前窗口状态为最大化时，还原窗口
-        if self.is_maximize:
-            UiFunctions.maximize_restore(self)
 
-        # 开始移动窗口
-        if event.buttons() == Qt.LeftButton:
-            self.move(self.pos() + event.globalPos() - self.dragPos)
-            self.dragPos = event.globalPos()
-            event.accept()
+def lock_message():
+        # 创建提示框
+        information_box = QMessageBox()
+        information_box.setWindowTitle("提示")
+        information_box.setText("程序已经启动！")
+        information_box.setIcon(QMessageBox.Information)
 
+        icon = QIcon()
+        icon.addFile(u":/images/icon/winlogo.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        information_box.setWindowIcon(icon)
+
+        # 添加按钮
+        btnYes = QPushButton("是")
+        information_box.addButton(btnYes, QMessageBox.ActionRole)
+
+        information_box.setFont(QFont("\u963f\u91cc\u5df4\u5df4\u666e\u60e0\u4f53 2.0 55 Regular", 12)) # 设置字体样式
+
+        information_box.exec()  # 等待操作
+
+# 运行程序
+def runWindow() -> None:
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("logo.ico"))    # 设置程序图标
+
+    # 创建文件锁，防止多开
+    temp_path = os.environ.get('TEMP')
+    lockFile = QLockFile(os.path.join(temp_path, "Translator.lock")) if temp_path else QLockFile("Translator.lock")
+
+    if lockFile.tryLock(250):
+        window = MainWindow()
+        sys.exit(app.exec())
+    else:
+        lock_message()
+        sys.exit(-1)
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("logo.ico"))
-    window = MainWindow()
-    sys.exit(app.exec())
+    runWindow()
